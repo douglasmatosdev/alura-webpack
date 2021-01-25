41eb8b5e27c9d30be0d81a4f400dc660a66ee27a
douglasmatosdev <douglasmatosdev@gmail.com>
2021-01-25 13:09:10 -0300

    Optimize closure process, optimize creating chuncks with separate artefacts with vendor and configure dynamic creatioin index.html with invalidation cash hash

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,10 +2,22 @@ const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWepackPlugin = require('html-webpack-plugin');
 
 const webpack = require('webpack');
 
 let plugins = []
+
+plugins.push(new HtmlWepackPlugin({
+    hash: true,
+    minify: {
+        html5: true,
+        collapseWhitespace: true,
+        removeComments: true
+    },
+    filename: 'index.html',
+    template: __dirname + '/main.html'
+}));
 
 plugins.push(new extractTextPlugin('styles.css'))
 
@@ -14,7 +26,15 @@ plugins.push(new webpack.ProvidePlugin({
     'jQuery': 'jquery/dist/jquery.js'
 }))
 
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.bundle.js'
+}))
+
 if (process.env.NODE_ENV == 'production') {
+
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
+    // plugins.push(new webpack.optimize.concatenateModules());
     plugins.push(new babiliPlugin());
 
     plugins.push(new optimizeCSSAssetsPlugin({
@@ -29,11 +49,14 @@ if (process.env.NODE_ENV == 'production') {
 }
 
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
+        // publicPath: 'dist'
     },
     module: {
         rules: [
